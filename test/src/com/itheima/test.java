b19d35d349febeefd90f2657a13803047bd6e14e
zhangjie <83840990@qq.com>
2018-12-27 08:46:43 +0800

    111

--- test/src/com/itheima/test.java
+++ test/src/com/itheima/test.java
@@ -4,4 +4,9 @@
             public static void main(String[] args) {
                 System.out.println("111");
             }
+
+            public void test2(){
+                int i = 0;
+                System.out.println("22222");
+            }
         }
